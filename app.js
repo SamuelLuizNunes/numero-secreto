ca79582b5b922b31bfae8cd318501d93a47172c8
Samuel Nunes <samuelluiznunes10@gmail.com>
2024-02-15 15:00:33 -0300

    alterando o valor limite

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -62,5 +62,5 @@ function reiniciarJogo() {
 
 function gerarTextoInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
