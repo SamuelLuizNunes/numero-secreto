1c61db814bea895634d49c6c1d36f0ea29a4dfd0
Samuel Nunes <samuelluiznunes10@gmail.com>
2024-02-18 13:04:04 -0300

    Deixando o jogo mais dificil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 6;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -62,5 +62,5 @@ function reiniciarJogo() {
 
 function gerarTextoInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 6');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
