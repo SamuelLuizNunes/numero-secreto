cd17c4db5ce1c144dcead0da4319ad41d06aa266
Samuel Nunes <samuelluiznunes10@gmail.com>
2024-02-18 15:48:43 -0300

    deixando o jogo facin

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 6;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -62,6 +62,6 @@ function reiniciarJogo() {
 
 function gerarTextoInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 6');
 }
 
